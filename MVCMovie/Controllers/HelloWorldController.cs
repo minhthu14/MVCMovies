b5dc4ea2e55fbb1d742bc26a48df3a455f792175
minhthu14 <mthubnheo@gmail.com>
2020-03-31 12:57:14 +0700

    Add project files.

---
+++ MVCMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MVCMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
